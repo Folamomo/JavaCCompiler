f2b23e65c802c66578ff5393b6c9372146092f59
Folamomo <noga.igor@gmail.com>
2020-03-06 14:32:19 +0100

    Initial commit

---
+++ src/test/java/pl/edu/agh/p810/compiler/MainTest.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.p810.compiler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void buildTest(){
+        assertTrue(true);
+    }
+}
